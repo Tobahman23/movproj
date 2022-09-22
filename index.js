20b4732f41dbecb763d394fcfd5bb31e616f8a25
Tobahman23 <tobias23.ta@gmail.com>
2022-09-22 15:02:17 +0200

    e
    e

--- index.js
+++ index.js
@@ -8,40 +8,40 @@ function changeco(){
     var buto = document.getElementById("but");
     const movi = document.querySelectorAll('.mov');
 
-    if (ligh.style.backgroundColor === 'burlywood'){
+    if (ligh.style.backgroundColor === 'rgba(180, 180, 180, 0.9)'){
 
-        ligh.style.backgroundColor = 'rgba(175, 0, 255, 0.9)';
+        ligh.style.backgroundColor = 'rgba(150, 0, 0, 0.9)';
         ligh.style.color = 'white';
-        ligh.style.borderColor = 'rgba(20,0,100)';
-        fotl.style.backgroundColor = 'rgba(175, 0, 255, 0.9)';
+        ligh.style.borderColor = 'rgba(80,0,0)';
+        fotl.style.backgroundColor = 'rgba(150, 0, 0, 0.9)';
         fotl.style.color = 'white';
-        fotl.style.borderColor = 'rgba(20,0,100)';
+        fotl.style.borderColor = 'rgba(80,0,0)';
         ligh.style.boxShadow = '0 0 1vh 0.5vh rgba(255, 255, 255, 0.8)';
         fotl.style.boxShadow = '0 0 1vh 0.5vh rgba(255, 255, 255, 0.8)';
         buto.value = "Toggle Light Mode";
 
         movi.forEach(mov => {
         mov.style.boxShadow = '0 0 1vh 0.5vh rgba(255, 255, 255, 0.8)';
-
+        document.body.style.backgroundImage = "url('red.png')";
         })
 
     }
 
     else{
 
-        ligh.style.backgroundColor = 'burlywood';
+        ligh.style.backgroundColor = 'rgba(180, 180, 180, 0.9)';
         ligh.style.color = 'black';
-        ligh.style.borderColor = 'black';   
-        fotl.style.backgroundColor = 'burlywood';
+        ligh.style.borderColor = 'rgba(100, 100, 100, 0.8)';   
+        fotl.style.backgroundColor = 'rgba(180, 180, 180, 0.9)';
         fotl.style.color = 'black';
-        fotl.style.borderColor = 'black';   
-        ligh.style.boxShadow = '0 0 1vh 0.5vh black';
-        fotl.style.boxShadow = '0 0 1vh 0.5vh black';
+        fotl.style.borderColor = 'rgba(100, 100, 100, 0.8)';   
+        ligh.style.boxShadow = '0 0 1vh 0.6vh black';
+        fotl.style.boxShadow = '0 0 1vh 0.6vh black';
         buto.value = "Toggle Dark Mode";
 
         movi.forEach(mov => {
-            mov.style.boxShadow = '0 0 1vh 0.5vh black';
-
+            mov.style.boxShadow = '0 0 1vh 0.6vh black';
+         document.body.style.backgroundImage = "url('')"
         })
     }
     
