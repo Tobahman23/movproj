543773ec985e67fd31bdc8e20bd75159b8e3d733
Tobahman23 <tobias23.ta@gmail.com>
2022-09-22 14:19:59 +0200

    min
    mini

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 
 function changeco(){
+
     var element = document.body;
     element.classList.toggle("lightmode");
     var ligh = document.getElementById("titl");
     var fotl = document.getElementById("foot");
     var buto = document.getElementById("but");
     const movi = document.querySelectorAll('.mov');
+
     if (ligh.style.backgroundColor === 'burlywood'){
+
         ligh.style.backgroundColor = 'rgba(175, 0, 255, 0.9)';
         ligh.style.color = 'white';
         ligh.style.borderColor = 'rgba(20,0,100)';
@@ -16,12 +19,16 @@ function changeco(){
         ligh.style.boxShadow = '0 0 1vh 0.5vh rgba(255, 255, 255, 0.8)';
         fotl.style.boxShadow = '0 0 1vh 0.5vh rgba(255, 255, 255, 0.8)';
         buto.value = "Toggle Light Mode";
+        
         movi.forEach(mov => {
-            mov.style.boxShadow = '0 0 1vh 0.5vh rgba(255, 255, 255, 0.8)';
+        mov.style.boxShadow = '0 0 1vh 0.5vh rgba(255, 255, 255, 0.8)';
+
         })
 
     }
+
     else{
+
         ligh.style.backgroundColor = 'burlywood';
         ligh.style.color = 'black';
         ligh.style.borderColor = 'black';   
@@ -31,8 +38,10 @@ function changeco(){
         ligh.style.boxShadow = '0 0 1vh 0.5vh rgba(80, 0, 220, 0.8)';
         fotl.style.boxShadow = '0 0 1vh 0.5vh rgba(80, 0, 220, 0.8)';
         buto.value = "Toggle Dark Mode";
+
         movi.forEach(mov => {
             mov.style.boxShadow = '0 0 1vh 0.5vh rgba(80, 0, 220, 0.8)';
+
         })
     }
     
