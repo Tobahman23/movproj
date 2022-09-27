5fa335be5711917e0e779fee2e51bfef9a72d134
Tobahman23 <tobias23.ta@gmail.com>
2022-09-27 15:56:13 +0200

    imfastasfboiii
    yee

--- index.js
+++ index.js
@@ -1,92 +1,95 @@
-const movieSearchBox = document.getElementById('movie-search-box');
-const searchList = document.getElementById('search-list');
-const resultGrid = document.getElementById('result-grid');
+const sea = document.getElementById('ser');
+const searching = document.getElementById('searchlist');
+const resg = document.getElementById('resultg');
 
-// load movies from API
-async function loadMovies(searchTerm){
-    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=d64588a9`;
-    const res = await fetch(`${URL}`);
-    const data = await res.json();
-    // console.log(data.Search);
-    if(data.Response == "True") displayMovieList(data.Search);
+async function loadMov(seter){
+    const URL = `https://omdbapi.com/?s=${seter}&page=1&apikey=d64588a9`; //gör en variabel som lagrar api länk som det sedan under kan hämtas data ifrån, detta ifall det man söker på har någon relevans med datan
+    const res = await fetch(`${URL}`); //response från länken
+    const data = await res.json(); //datan ifrån apin
+    if(data.Response == "True") displayMov(data.Search); //Om den får en response så körs Funktionen displayMov
 }
 
-function findMovies(){
-    let searchTerm = (movieSearchBox.value).trim();
-    if(searchTerm.length > 0){
-        searchList.classList.remove('hide-search-list');
-        loadMovies(searchTerm);
+function findMov(){
+    let seter = (sea.value).trim();
+    if(seter.length > 0){
+        searching.classList.remove('hides');
+        loadMov(seter);
     } else {
-        searchList.classList.add('hide-search-list');
+        searching.classList.add('hides');
     }
 }
 
-function displayMovieList(movies){
-    searchList.innerHTML = "";
+function displayMov(movies){
+    searching.innerHTML = "";
     for(let idx = 0; idx < movies.length; idx++){
         let movieListItem = document.createElement('div');
-        movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
-        movieListItem.classList.add('search-list-item');
+        movieListItem.dataset.id = movies[idx].imdbID; 
+        movieListItem.classList.add('searcher');
         if(movies[idx].Poster != "N/A")
             moviePoster = movies[idx].Poster;
         else 
             moviePoster = "image_not_found.png";
 
         movieListItem.innerHTML = `
-        <div class = "search-item-thumbnail">
+        <div class = "serimg">
             <img src = "${moviePoster}">
         </div>
-        <div class = "search-item-info">
+        <div class = "serinf">
             <h3>${movies[idx].Title}</h3>
             <p>${movies[idx].Year}</p>
         </div>
         `;
-        searchList.appendChild(movieListItem);
+        searching.appendChild(movieListItem);
     }
-    loadMovieDetails();
+    loadmovdet();
 }
 
-function loadMovieDetails(){
-    const searchListMovies = searchList.querySelectorAll('.search-list-item');
-    searchListMovies.forEach(movie => {
+function loadmovdet(){
+    const smovie = searching.querySelectorAll('.searcher');
+    smovie.forEach(movie => {
         movie.addEventListener('click', async () => {
-            // console.log(movie.dataset.id);
-            searchList.classList.add('hide-search-list');
-            movieSearchBox.value = "";
+            searching.classList.add('hides');
+            sea.value = "";
             const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=d64588a9`);
             const movieDetails = await result.json();
-            // console.log(movieDetails);
-            displayMovieDetails(movieDetails);
+            dismov(movieDetails);
         });
     });
 }
 
-function displayMovieDetails(details){
-    resultGrid.innerHTML = `
-    <div class = "movie-poster">
+function dismov(details){
+    resg.innerHTML = `
+    <div class = "movpos">
         <img src = "${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt = "movie poster">
     </div>
-    <div class = "movie-info">
-        <h3 class = "movie-title">${details.Title}</h3>
-        <ul class = "movie-misc-info">
+    <div class = "movinf">
+        <h4 class = "movtit">${details.Title}</h4>
+        <ul class = "movlis">
             <li class = "year">Year: ${details.Year}</li>
-            <li class = "rated">Ratings: ${details.Rated}</li>
+            <li class = "rated">Rated: ${details.Rated}</li>
             <li class = "released">Released: ${details.Released}</li>
         </ul>
+        <br>
         <p class = "genre"><b>Genre:</b> ${details.Genre}</p>
+        <br>
+        <p class = "ratings"<b>Imdb Rating:</b> ${details.imdbRating}</p>
+        <br>
         <p class = "writer"><b>Writer:</b> ${details.Writer}</p>
+        <br>
         <p class = "actors"><b>Actors: </b>${details.Actors}</p>
+        <br>
         <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
+        <br>
         <p class = "language"><b>Language:</b> ${details.Language}</p>
+        <br>
         <p class = "awards"><b><i class = "fas fa-award"></i></b> ${details.Awards}</p>
     </div>
     `;
 }
 
-
 window.addEventListener('click', (event) => {
-    if(event.target.className != "form-control"){
-        searchList.classList.add('hide-search-list');
+    if(event.target.className != "inpt"){
+        searching.classList.add('hides');
     }
 });
 
@@ -98,7 +101,7 @@ function changeco(){
     var ligh = document.getElementById("titl");
     var fotl = document.getElementById("foot");
     var buto = document.getElementById("but");
-    const movi = document.querySelectorAll('.mov');
+    const movi = document.querySelectorAll('.movinf');
 
     if (ligh.style.backgroundColor === 'rgba(180, 180, 180, 0.9)'){
 
@@ -115,9 +118,8 @@ function changeco(){
         buto.style.color = 'black';
         buto.style.borderColor = 'black';
 
-        movi.forEach(mov => {
-        mov.style.boxShadow = '0 0 1vh 0.5vh rgba(255, 255, 255, 0.8)';
-        document.body.style.backgroundImage = "url('red.png')";
+        movi.forEach(movinf => {
+            movinf.style.backgroundColor = 'rgba(150, 0, 0, 0.9)';
         })
 
     }
@@ -136,10 +138,10 @@ function changeco(){
         buto.style.backgroundColor = 'black';
         buto.style.color = 'white';
         buto.style.borderColor = 'white';
+        
 
-        movi.forEach(mov => {
-            mov.style.boxShadow = '0 0 1vh 0.6vh black';
-         document.body.style.backgroundImage = "url('pur.png')";
+        movi.forEach(movinf => {
+            movinf.style.backgroundColor = 'rgba(180, 180, 180, 0.9)';
         })
     }
     
