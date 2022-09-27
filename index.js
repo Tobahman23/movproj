293b6ac710788396ac47e29722fb1fa844d3c126
Tobahman23 <tobias23.ta@gmail.com>
2022-09-27 09:07:54 +0200

    e
    e

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-fetch('https://api-gate.movieglu.com/kbUN4twaUE8zz0zyMaLOp7HGgom68Gfv15DZOHjK')
-{
+var deviceDateTime = new Date().toLocaleString();
 
-}
+var settings = {
+    "crossDomain": true,
+    "url": "https://api-gate2.movieglu.com/filmsNowShowing/?n=10",
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+    "api-version": "v200",
+    "Authorization": "Basic UEIQRV9YWDpiNGIFdINLTXVXdWs=",
+    "client": "PIPE",
+    "x-api-key": "v77F9YdobF6DHVd2TJpVhadfmvNnLrujPq8M4jJc",
+    "device-datetime": `${deviceDateTime}`,
+    "territory": "SE",
+    },
+    };
+    
+    $.ajax(settings).done(function (response) {
+    console.log(response);
+    });
 
 function changeco(){
 
@@ -23,6 +39,9 @@ function changeco(){
         ligh.style.boxShadow = '0 0 1vh 0.5vh rgba(255, 255, 255, 0.8)';
         fotl.style.boxShadow = '0 0 1vh 0.5vh rgba(255, 255, 255, 0.8)';
         buto.value = "Toggle Light Mode";
+        buto.style.backgroundColor = 'white';
+        buto.style.color = 'black';
+        buto.style.borderColor = 'black';
 
         movi.forEach(mov => {
         mov.style.boxShadow = '0 0 1vh 0.5vh rgba(255, 255, 255, 0.8)';
@@ -42,6 +61,9 @@ function changeco(){
         ligh.style.boxShadow = '0 0 1vh 0.6vh black';
         fotl.style.boxShadow = '0 0 1vh 0.6vh black';
         buto.value = "Toggle Dark Mode";
+        buto.style.backgroundColor = 'black';
+        buto.style.color = 'white';
+        buto.style.borderColor = 'white';
 
         movi.forEach(mov => {
             mov.style.boxShadow = '0 0 1vh 0.6vh black';
